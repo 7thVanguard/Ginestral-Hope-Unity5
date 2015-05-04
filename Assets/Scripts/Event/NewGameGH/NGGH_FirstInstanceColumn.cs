1615eb42c853f6abcab52452f597caacddb14f86
Palaui <sucdepressec@gmail.com>
2015-05-04 23:28:04 +0200

    Animations Reset done

--- Assets/Scripts/Event/NewGameGH/NGGH_FirstInstanceColumn.cs
+++ Assets/Scripts/Event/NewGameGH/NGGH_FirstInstanceColumn.cs
@@ -4,16 +4,29 @@ using System.Collections;
 public class NGGH_FirstInstanceColumn : MonoBehaviour 
 {
     public GameObject column;
-    private bool finished = false;
+    private bool finish = false;
+
+
+    void Update()
+    {
+        // Reset
+        if (GameFlow.resetState == GameFlow.ResetState.Reset)
+        {
+            column.GetComponent<Animation>()["Take 001"].speed = -1;
+            column.GetComponent<Animation>().Play();
+            finish = false;
+        }
+    }
 
 
     void OnTriggerStay(Collider other)
     {
-        if (!finished)
+        if (!finish)
             if (other.tag == "Player")
             {
+                column.GetComponent<Animation>()["Take 001"].speed = 1;
                 column.GetComponent<Animation>().Play();
-                finished = true;
+                finish = true;
             }
     }
 }
--- Assets/Scripts/Event/NewGameGH/NGGH_ThirdInstanceWagon.cs
+++ Assets/Scripts/Event/NewGameGH/NGGH_ThirdInstanceWagon.cs
@@ -5,30 +5,26 @@ using System.Collections.Generic;
 public class NGGH_ThirdInstanceWagon : MonoBehaviour
 {
     private Animation animator;
-    private float counter = 0;
-    private float timeToCount = 3;
 
-    private bool animationDone = false;     
-    private bool ready;                     // When ready to pick up the light
     private bool inTrigger = false;
+    private bool finish = false;
 	
 
 	void Start() 
 	{
 		animator = transform.parent.GetComponent<Animation>();
-		animator ["Anim01"].speed = 0.5f;
+		animator["Anim01"].speed = 0.5f;
 	}
 
 
     void Update()
     {
-        // Count three seconds from pushing the minecrat before we can pick up the light
-        if (animationDone && !ready)
+        // Reset
+        if (GameFlow.resetState == GameFlow.ResetState.Reset)
         {
-            counter += Time.deltaTime;
-
-            if (counter >= timeToCount)
-                ready = true;
+            animator["Anim01"].speed = -1;
+            animator.Play();
+            finish = false;
         }
     }
 
@@ -37,26 +33,15 @@ public class NGGH_ThirdInstanceWagon : MonoBehaviour
     {
         inTrigger = true;
 
-        if (other.tag == "Player")
-        {
-            if (Input.GetKey(KeyCode.E))
-            {
-                // Push the MineCart
-                if (!animationDone)
+        if (!finish)
+            if (other.tag == "Player")
+                if (Input.GetKey(KeyCode.E))
                 {
-                    transform.parent.GetComponent<Animation>().Play();
-                    animationDone = true;
+                    // Push the MineCart
+                    animator["Anim01"].speed = 0.5f;
+                    animator.Play();
+                    finish = true;
                 }
-                // Player pick up light
-                else if (ready)
-                {
-                    GameObject lamp = transform.parent.FindChild("Lamp").gameObject;
-                    lamp.transform.parent = Global.player.playerObj.transform.FindChild("Mesh");
-                    lamp.transform.localPosition = new Vector3(0.3f, 0.5f, 0);
-                    Destroy(transform.GetComponent<NGGH_ThirdInstanceWagon>());
-                }
-            }
-        }
     }
 
 
